0bd5c0c5282a48c6ed1d6141f34c5600f41e3940
Max Carrión <maxmateo.carrion@epn.edu.ec>
2024-12-02 12:45:23 -0500

    change: route path

--- src/App.tsx
+++ src/App.tsx
@@ -9,8 +9,8 @@ const App: React.FC = () => {
     return (
         <Router>
             <Routes>
-                <Route path="/" element={<WelcomeScreen />} />
-                <Route path="/levels" element={<LevelSelection />} />
+                <Route path="/" element={<LevelSelection />} />
+                <Route path="/levels" element={<WelcomeScreen />} />
                 <Route path="/questions" element={<QuestionScreen />} />
                 <Route path="/final" element={<FinalScreen />} />
             </Routes>
